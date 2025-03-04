af2f43ca237ad3b50704c3e3e4579c21bcec4023
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-03-05 02:45:18 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,6 +1,5 @@
 public class Circle implements Shape{
-    @Override
     public void draw(){
-        System.out.println("Circle : draw()");
+        System.out.println("Circle: draw()");
     }
 }
