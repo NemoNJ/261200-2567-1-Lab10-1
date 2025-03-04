09296a27a9d581afa139620fdcb7a034efeeffd2
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-03-05 02:50:53 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,23 +8,23 @@ public class Main {
         if (shape2 != null)
             shape2.draw();
 
-        Shape shape3 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
+        Shape shape3 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
         if (shape3 != null)
             shape3.draw();
 
-        Shape shape4 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
+        Shape shape4 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
         if (shape4 != null)
             shape4.draw();
 
-        Shape shape5 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
+        Shape shape5 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
         if (shape5 != null)
             shape5.draw();
 
-        Shape shape6 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
+        Shape shape6 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
         if (shape6 != null)
             shape6.draw();
 
-        Shape shape7 = ShapeFactory.getShape(ShapeFactory.TYPE_RECTANGLE);
+        Shape shape7 = ShapeFactory.getShape(ShapeFactory.TYPE_SQUARE);
         if (shape7 != null)
             shape7.draw();
     }
