5d4365260f8155ca74f43afc3fcb7551abaaa6ac
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-03-05 02:46:43 +0700

    Update ShapeFactory.java

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,44 +1,49 @@
 public class ShapeFactory {
-    //กำหนดจำนวนการ spawn ได้
     public static final int TYPE_CIRCLE = 0;
     public static final int TYPE_RECTANGLE = 1;
-    public static final int TYPE_SQUARE= 2;
-    private static int countCircle = 0;
-    private static final int countCircleLimit = 2;
-    private static int countRectangle = 0;
-    private static final int countRectangleLimit = 2;
-    private static int countSquare = 0;
-    private static final int countSquareLimit = 2;
-    private static int countAll = 0;
-    private static final int countAllLimit = 5;
-     public static Shape getShape(int type){
-         switch(type){
-             case TYPE_CIRCLE:
-                 if(countCircle < countCircleLimit && countAll < countAllLimit){
-                     countCircle++;
-                     countAll++;
-                     return new Circle();
-                 } else{
-                     return null;
-                 }
-             case TYPE_RECTANGLE :
-                 if(countRectangle < countRectangleLimit && countAll < countAllLimit){
-                     countRectangle++;
-                     countAll++;
-                     return  new Rectangle();
-                 } else{
-                     return null;
-                 }
-             case TYPE_SQUARE :
-                 if(countSquare < countSquareLimit && countAll < countAllLimit){
-                     countSquare++;
-                     countAll++;
-                     return  new Square();
-                 } else{
-                     return null;
-                 }
-             default :
-                 return null;
-         }
-     }
+    public static final int TYPE_SQUARE = 2;
+
+    private static final int MAX_EACH_SHAPE = 2;
+    private static final int MAX_TOTAL_SHAPES = 5;
+
+    private static int circleCount = 0;
+    private static int rectangleCount = 0;
+    private static int squareCount = 0;
+    private static int totalShapes = 0;
+
+    public static Shape getShape(int type){
+        if (totalShapes >= MAX_TOTAL_SHAPES) {
+            System.out.println("Cannot create more shapes. Maximum limit reached!");
+            return null;
+        }
+
+        switch (type) {
+            case TYPE_CIRCLE:
+                if (circleCount < MAX_EACH_SHAPE) {
+                    circleCount++;
+                    totalShapes++;
+                    return new Circle();
+                }
+                break;
+            case TYPE_RECTANGLE:
+                if (rectangleCount < MAX_EACH_SHAPE) {
+                    rectangleCount++;
+                    totalShapes++;
+                    return new Rectangle();
+                }
+                break;
+            case TYPE_SQUARE:
+                if (squareCount < MAX_EACH_SHAPE) {
+                    squareCount++;
+                    totalShapes++;
+                    return new Square();
+                }
+                break;
+            default:
+                System.out.println("Invalid shape type");
+                return null;
+        }
+        System.out.println("Cannot create more of this shape type. Limit reached!");
+        return null;
+    }
 }
